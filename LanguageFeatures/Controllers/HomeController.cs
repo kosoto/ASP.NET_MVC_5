12335ab6c2b761ab1a649e9ba28ba4208ace9561
kosoto <kstad@naver.com>
2018-12-18 23:15:58 +0900

    12.18 23:15

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            // create a new Product object
+            Product myProduct = new Product();
+            // set the property value
+            myProduct.Name = "Kayak";
+            // get the property
+            string productName = myProduct.Name;
+            // generate the view
+            return View("Result", (object)String.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            // create and populate a new Product object
+            //initializer
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+            return View("Result",
+                (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int> {
+                { "apple", 10 }, { "orange", 20 }, { "plum", 30 }
+            };
+            return View("Result", (object)stringArray[1]);
+        }
+    }
+}
